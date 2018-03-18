c9384601ae3fde1a1c82eb99523d027237d8dc12
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-03-17 20:12:15 -0400

    Add files

---
+++ Structures/src/Graph.cpp.d
@@ -0,0 +1,378 @@
+Structures/src/Graph.cpp.o: Structures/src/Graph.cpp \
+ /usr/include/stdc-predef.h Structures/Graph.hpp Utils/Mathf.hpp \
+ ../../dpf/dgl/Geometry.hpp ../../dpf/dgl/Base.hpp \
+ ../../dpf/dgl/../distrho/extra/LeakDetector.hpp \
+ ../../dpf/dgl/../distrho/extra/../DistrhoUtils.hpp \
+ ../../dpf/dgl/../distrho/extra/../src/DistrhoDefines.h \
+ /usr/include/c++/7.3.0/cstdarg \
+ /usr/include/c++/7.3.0/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/7.3.0/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/7.3.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include/stdarg.h \
+ /usr/include/c++/7.3.0/cstdio /usr/include/stdio.h \
+ /usr/include/bits/libc-header-start.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/types/__FILE.h /usr/include/bits/types/FILE.h \
+ /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/stdio_lim.h \
+ /usr/include/bits/sys_errlist.h /usr/include/bits/stdio.h \
+ /usr/include/bits/stdio2.h /usr/include/c++/7.3.0/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/bits/floatn.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/timer_t.h /usr/include/bits/stdint-intn.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/select2.h \
+ /usr/include/sys/sysmacros.h /usr/include/bits/sysmacros.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-bsearch.h /usr/include/bits/stdlib-float.h \
+ /usr/include/bits/stdlib.h /usr/include/c++/7.3.0/bits/std_abs.h \
+ /usr/include/c++/7.3.0/cstring /usr/include/string.h \
+ /usr/include/strings.h /usr/include/bits/strings_fortified.h \
+ /usr/include/bits/string_fortified.h /usr/include/c++/7.3.0/cmath \
+ /usr/include/c++/7.3.0/bits/cpp_type_traits.h \
+ /usr/include/c++/7.3.0/ext/type_traits.h /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/huge_val.h /usr/include/bits/huge_val_flt128.h \
+ /usr/include/bits/huge_valf.h /usr/include/bits/huge_vall.h \
+ /usr/include/bits/inf.h /usr/include/bits/nan.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/iscanonical.h \
+ /usr/include/bits/mathinline.h /usr/include/bits/math-finite.h \
+ /usr/include/c++/7.3.0/limits /usr/include/c++/7.3.0/cstdint \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-uintn.h \
+ ../../dpf/dgl/../distrho/extra/ScopedPointer.hpp \
+ /usr/include/c++/7.3.0/algorithm /usr/include/c++/7.3.0/utility \
+ /usr/include/c++/7.3.0/bits/stl_relops.h \
+ /usr/include/c++/7.3.0/bits/stl_pair.h \
+ /usr/include/c++/7.3.0/bits/move.h \
+ /usr/include/c++/7.3.0/bits/concept_check.h \
+ /usr/include/c++/7.3.0/type_traits \
+ /usr/include/c++/7.3.0/initializer_list \
+ /usr/include/c++/7.3.0/bits/stl_algobase.h \
+ /usr/include/c++/7.3.0/bits/functexcept.h \
+ /usr/include/c++/7.3.0/bits/exception_defines.h \
+ /usr/include/c++/7.3.0/ext/numeric_traits.h \
+ /usr/include/c++/7.3.0/bits/stl_iterator_base_types.h \
+ /usr/include/c++/7.3.0/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/7.3.0/debug/assertions.h \
+ /usr/include/c++/7.3.0/bits/stl_iterator.h \
+ /usr/include/c++/7.3.0/bits/ptr_traits.h \
+ /usr/include/c++/7.3.0/debug/debug.h \
+ /usr/include/c++/7.3.0/bits/predefined_ops.h \
+ /usr/include/c++/7.3.0/bits/stl_algo.h \
+ /usr/include/c++/7.3.0/bits/algorithmfwd.h \
+ /usr/include/c++/7.3.0/bits/stl_heap.h \
+ /usr/include/c++/7.3.0/bits/stl_tempbuf.h \
+ /usr/include/c++/7.3.0/bits/stl_construct.h /usr/include/c++/7.3.0/new \
+ /usr/include/c++/7.3.0/exception /usr/include/c++/7.3.0/bits/exception.h \
+ /usr/include/c++/7.3.0/bits/exception_ptr.h \
+ /usr/include/c++/7.3.0/bits/cxxabi_init_exception.h \
+ /usr/include/c++/7.3.0/typeinfo /usr/include/c++/7.3.0/bits/hash_bytes.h \
+ /usr/include/c++/7.3.0/bits/nested_exception.h \
+ /usr/include/c++/7.3.0/ext/alloc_traits.h \
+ /usr/include/c++/7.3.0/bits/alloc_traits.h \
+ /usr/include/c++/7.3.0/bits/memoryfwd.h \
+ /usr/include/c++/7.3.0/bits/uniform_int_dist.h /usr/include/GL/gl.h \
+ /usr/include/GL/glext.h /usr/include/inttypes.h \
+ /usr/include/c++/7.3.0/cassert /usr/include/assert.h
+
+/usr/include/stdc-predef.h:
+
+Structures/Graph.hpp:
+
+Utils/Mathf.hpp:
+
+../../dpf/dgl/Geometry.hpp:
+
+../../dpf/dgl/Base.hpp:
+
+../../dpf/dgl/../distrho/extra/LeakDetector.hpp:
+
+../../dpf/dgl/../distrho/extra/../DistrhoUtils.hpp:
+
+../../dpf/dgl/../distrho/extra/../src/DistrhoDefines.h:
+
+/usr/include/c++/7.3.0/cstdarg:
+
+/usr/include/c++/7.3.0/x86_64-pc-linux-gnu/bits/c++config.h:
+
+/usr/include/c++/7.3.0/x86_64-pc-linux-gnu/bits/os_defines.h:
+
+/usr/include/features.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/bits/wordsize.h:
+
+/usr/include/bits/long-double.h:
+
+/usr/include/gnu/stubs.h:
+
+/usr/include/gnu/stubs-64.h:
+
+/usr/include/c++/7.3.0/x86_64-pc-linux-gnu/bits/cpu_defines.h:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include/stdarg.h:
+
+/usr/include/c++/7.3.0/cstdio:
+
+/usr/include/stdio.h:
+
+/usr/include/bits/libc-header-start.h:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include/stddef.h:
+
+/usr/include/bits/types.h:
+
+/usr/include/bits/typesizes.h:
+
+/usr/include/bits/types/__FILE.h:
+
+/usr/include/bits/types/FILE.h:
+
+/usr/include/libio.h:
+
+/usr/include/_G_config.h:
+
+/usr/include/bits/types/__mbstate_t.h:
+
+/usr/include/bits/stdio_lim.h:
+
+/usr/include/bits/sys_errlist.h:
+
+/usr/include/bits/stdio.h:
+
+/usr/include/bits/stdio2.h:
+
+/usr/include/c++/7.3.0/cstdlib:
+
+/usr/include/stdlib.h:
+
+/usr/include/bits/waitflags.h:
+
+/usr/include/bits/waitstatus.h:
+
+/usr/include/bits/floatn.h:
+
+/usr/include/bits/types/locale_t.h:
+
+/usr/include/bits/types/__locale_t.h:
+
+/usr/include/sys/types.h:
+
+/usr/include/bits/types/clock_t.h:
+
+/usr/include/bits/types/clockid_t.h:
+
+/usr/include/bits/types/time_t.h:
+
+/usr/include/bits/types/timer_t.h:
+
+/usr/include/bits/stdint-intn.h:
+
+/usr/include/endian.h:
+
+/usr/include/bits/endian.h:
+
+/usr/include/bits/byteswap.h:
+
+/usr/include/bits/byteswap-16.h:
+
+/usr/include/bits/uintn-identity.h:
+
+/usr/include/sys/select.h:
+
+/usr/include/bits/select.h:
+
+/usr/include/bits/types/sigset_t.h:
+
+/usr/include/bits/types/__sigset_t.h:
+
+/usr/include/bits/types/struct_timeval.h:
+
+/usr/include/bits/types/struct_timespec.h:
+
+/usr/include/bits/select2.h:
+
+/usr/include/sys/sysmacros.h:
+
+/usr/include/bits/sysmacros.h:
+
+/usr/include/bits/pthreadtypes.h:
+
+/usr/include/bits/thread-shared-types.h:
+
+/usr/include/bits/pthreadtypes-arch.h:
+
+/usr/include/alloca.h:
+
+/usr/include/bits/stdlib-bsearch.h:
+
+/usr/include/bits/stdlib-float.h:
+
+/usr/include/bits/stdlib.h:
+
+/usr/include/c++/7.3.0/bits/std_abs.h:
+
+/usr/include/c++/7.3.0/cstring:
+
+/usr/include/string.h:
+
+/usr/include/strings.h:
+
+/usr/include/bits/strings_fortified.h:
+
+/usr/include/bits/string_fortified.h:
+
+/usr/include/c++/7.3.0/cmath:
+
+/usr/include/c++/7.3.0/bits/cpp_type_traits.h:
+
+/usr/include/c++/7.3.0/ext/type_traits.h:
+
+/usr/include/math.h:
+
+/usr/include/bits/math-vector.h:
+
+/usr/include/bits/libm-simd-decl-stubs.h:
+
+/usr/include/bits/huge_val.h:
+
+/usr/include/bits/huge_val_flt128.h:
+
+/usr/include/bits/huge_valf.h:
+
+/usr/include/bits/huge_vall.h:
+
+/usr/include/bits/inf.h:
+
+/usr/include/bits/nan.h:
+
+/usr/include/bits/flt-eval-method.h:
+
+/usr/include/bits/fp-logb.h:
+
+/usr/include/bits/fp-fast.h:
+
+/usr/include/bits/mathcalls-helper-functions.h:
+
+/usr/include/bits/mathcalls.h:
+
+/usr/include/bits/iscanonical.h:
+
+/usr/include/bits/mathinline.h:
+
+/usr/include/bits/math-finite.h:
+
+/usr/include/c++/7.3.0/limits:
+
+/usr/include/c++/7.3.0/cstdint:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/bits/wchar.h:
+
+/usr/include/bits/stdint-uintn.h:
+
+../../dpf/dgl/../distrho/extra/ScopedPointer.hpp:
+
+/usr/include/c++/7.3.0/algorithm:
+
+/usr/include/c++/7.3.0/utility:
+
+/usr/include/c++/7.3.0/bits/stl_relops.h:
+
+/usr/include/c++/7.3.0/bits/stl_pair.h:
+
+/usr/include/c++/7.3.0/bits/move.h:
+
+/usr/include/c++/7.3.0/bits/concept_check.h:
+
+/usr/include/c++/7.3.0/type_traits:
+
+/usr/include/c++/7.3.0/initializer_list:
+
+/usr/include/c++/7.3.0/bits/stl_algobase.h:
+
+/usr/include/c++/7.3.0/bits/functexcept.h:
+
+/usr/include/c++/7.3.0/bits/exception_defines.h:
+
+/usr/include/c++/7.3.0/ext/numeric_traits.h:
+
+/usr/include/c++/7.3.0/bits/stl_iterator_base_types.h:
+
+/usr/include/c++/7.3.0/bits/stl_iterator_base_funcs.h:
+
+/usr/include/c++/7.3.0/debug/assertions.h:
+
+/usr/include/c++/7.3.0/bits/stl_iterator.h:
+
+/usr/include/c++/7.3.0/bits/ptr_traits.h:
+
+/usr/include/c++/7.3.0/debug/debug.h:
+
+/usr/include/c++/7.3.0/bits/predefined_ops.h:
+
+/usr/include/c++/7.3.0/bits/stl_algo.h:
+
+/usr/include/c++/7.3.0/bits/algorithmfwd.h:
+
+/usr/include/c++/7.3.0/bits/stl_heap.h:
+
+/usr/include/c++/7.3.0/bits/stl_tempbuf.h:
+
+/usr/include/c++/7.3.0/bits/stl_construct.h:
+
+/usr/include/c++/7.3.0/new:
+
+/usr/include/c++/7.3.0/exception:
+
+/usr/include/c++/7.3.0/bits/exception.h:
+
+/usr/include/c++/7.3.0/bits/exception_ptr.h:
+
+/usr/include/c++/7.3.0/bits/cxxabi_init_exception.h:
+
+/usr/include/c++/7.3.0/typeinfo:
+
+/usr/include/c++/7.3.0/bits/hash_bytes.h:
+
+/usr/include/c++/7.3.0/bits/nested_exception.h:
+
+/usr/include/c++/7.3.0/ext/alloc_traits.h:
+
+/usr/include/c++/7.3.0/bits/alloc_traits.h:
+
+/usr/include/c++/7.3.0/bits/memoryfwd.h:
+
+/usr/include/c++/7.3.0/bits/uniform_int_dist.h:
+
+/usr/include/GL/gl.h:
+
+/usr/include/GL/glext.h:
+
+/usr/include/inttypes.h:
+
+/usr/include/c++/7.3.0/cassert:
+
+/usr/include/assert.h:
---
+++ Structures/src/Layout.cpp.d
@@ -0,0 +1,9 @@
+Structures/src/Layout.cpp.o: Structures/src/Layout.cpp \
+ /usr/include/stdc-predef.h Structures/Layout.hpp \
+ ../../dpf/distrho/src/DistrhoDefines.h
+
+/usr/include/stdc-predef.h:
+
+Structures/Layout.hpp:
+
+../../dpf/distrho/src/DistrhoDefines.h:
