0bb466d86f0fbf6e6134cce8126c14b44cdbbd50
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-03-20 08:05:40 -0400

    Remove unwanted files

--- Structures/src/Layout.cpp.d
+++
@@ -1,9 +0,0 @@
-Structures/src/Layout.cpp.o: Structures/src/Layout.cpp \
- /usr/include/stdc-predef.h Structures/Layout.hpp \
- ../../dpf/distrho/src/DistrhoDefines.h
-
-/usr/include/stdc-predef.h:
-
-Structures/Layout.hpp:
-
-../../dpf/distrho/src/DistrhoDefines.h:
