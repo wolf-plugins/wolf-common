eaeab752e8d25e244c763a1d259d3900212a8356
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-04-07 22:06:13 -0400

    Initial work on proper skin for reset button

--- Widgets/src/ResetGraphButton.cpp
+++ Widgets/src/ResetGraphButton.cpp
@@ -12,15 +12,29 @@ ResetGraphButton::ResetGraphButton(NanoWidget *widget, Size<uint> size) noexcept
 
 void ResetGraphButton::drawUp()
 {
+    const float width = getWidth();
+    const float height = getHeight();
+
+    const float halfWidth = width / 2.0f;
+    const float halfHeight = height / 2.0f;
+
+    //arrow
     beginPath();
 
-    strokeWidth(2.0f);
-    fillColor(Color(255,0,0,255));
-    strokeColor(Color(0,0,0,255));
+    strokeWidth(3.0f);
+    strokeColor(Color(255, 255, 255, 255));
 
-    rect(0, 0, getWidth(), getHeight());
+    const float margin = 6.0f;
+    const float halfMargin = margin / 2.0f;
 
-    fill();
+    const float radius = halfWidth - margin;
+
+    arc(halfWidth, halfHeight, radius, 3.47321f, 2.98451f, NanoVG::Winding::CW);
+
+    moveTo(margin, margin);
+    lineTo(margin, radius + halfMargin);
+    lineTo(radius + halfMargin, radius + halfMargin);
+
     stroke();
 
     closePath();
@@ -28,31 +42,23 @@ void ResetGraphButton::drawUp()
 
 void ResetGraphButton::drawDown()
 {
-    beginPath();
-    
-    strokeWidth(2.0f);
-    fillColor(Color(0,0,255,255));
-    strokeColor(Color(0,0,0,255));
-
-    rect(0, 0, getWidth(), getHeight());
-
-    fill();
-    stroke();
-
-    closePath();
+    drawHover();
 }
 
-void ResetGraphButton::drawHover() 
+void ResetGraphButton::drawHover()
 {
+    drawUp();
+
     beginPath();
-    
+
     strokeWidth(2.0f);
-    fillColor(Color(0,255,0,255));
-    strokeColor(Color(0,0,0,255));
+    strokeColor(Color(255, 255, 255, 255));
 
-    rect(0, 0, getWidth(), getHeight());
+    const float margin = 2.0f;
+    const float doubleMargin = margin * 2.0f;
 
-    fill();
+    roundedRect(margin, margin, getWidth() - doubleMargin, getHeight() - doubleMargin, 4.0f);
+
     stroke();
 
     closePath();
