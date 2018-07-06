6bf5882596e9b29ac04bea5b31af2057d945cb05
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-07-06 12:46:25 -0400

    Use VCVRack's code for drawing nanosvg parsed data

--- Utils/SVGUtils.hpp
+++ Utils/SVGUtils.hpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include <cmath>
 #include <cstdint>
-
+#include <cassert>
 #include "nanosvg.h"
 #include "nanovg.h"
 
@@ -14,9 +14,65 @@ START_NAMESPACE_DISTRHO
 namespace SVGUtils
 {
 
-NVGcolor nvgColorU32(uint32_t color);
-void nvgSVGLinearGrad(NVGcontext *vg, NSVGgradient *grad);
-void nvgDrawSVG(NVGcontext *vg, NSVGimage *image);
+//based on VCVRack's code: https://github.com/VCVRack/Rack/blob/bd4f68c13d0bb8577aa1fb35c25b89da63f062af/include/util/math.hpp
+struct Vec {
+	float x = 0.f;
+	float y = 0.f;
+
+	Vec() {}
+	Vec(float x, float y) : x(x), y(y) {}
+
+	Vec neg() {
+		return Vec(-x, -y);
+	}
+	Vec plus(Vec b) {
+		return Vec(x + b.x, y + b.y);
+	}
+	Vec minus(Vec b) {
+		return Vec(x - b.x, y - b.y);
+	}
+	Vec mult(float s) {
+		return Vec(x * s, y * s);
+	}
+	Vec mult(Vec b) {
+		return Vec(x * b.x, y * b.y);
+	}
+	Vec div(float s) {
+		return Vec(x / s, y / s);
+	}
+	Vec div(Vec b) {
+		return Vec(x / b.x, y / b.y);
+	}
+	float dot(Vec b) {
+		return x * b.x + y * b.y;
+	}
+	float norm() {
+		return hypotf(x, y);
+	}
+	Vec flip() {
+		return Vec(y, x);
+	}
+	Vec round() {
+		return Vec(roundf(x), roundf(y));
+	}
+	Vec floor() {
+		return Vec(floorf(x), floorf(y));
+	}
+	Vec ceil() {
+		return Vec(ceilf(x), ceilf(y));
+	}
+	bool isEqual(Vec b) {
+		return x == b.x && y == b.y;
+	}
+	bool isZero() {
+		return x == 0.0f && y == 0.0f;
+	}
+};
+
+NVGcolor getNVGColor(uint32_t color);
+float getLineCrossing(Vec p0, Vec p1, Vec p2, Vec p3);
+NVGpaint getPaint(NVGcontext *vg, NSVGpaint *p);
+void nvgDrawSVG(NVGcontext *vg, NSVGimage *svg);
 
 };
 
--- Utils/src/SVGUtils.cpp
+++ Utils/src/SVGUtils.cpp
@@ -10,74 +10,180 @@ START_NAMESPACE_DISTRHO
 namespace SVGUtils
 {
 
-NVGcolor nvgColorU32(uint32_t color)
+//based on VCVRack's code: https://raw.githubusercontent.com/VCVRack/Rack/v0.6/src/widgets/SVGWidget.cpp
+
+NVGcolor getNVGColor(uint32_t color)
 {
-    return nvgRGB(color & 0xFF, (color >> 8) & 0xFF, (color >> 16) & 0xFF);
+    return nvgRGBA(
+        (color >> 0) & 0xff,
+        (color >> 8) & 0xff,
+        (color >> 16) & 0xff,
+        (color >> 24) & 0xff);
 }
 
-void nvgSVGLinearGrad(NVGcontext *vg, NSVGgradient *grad)
+NVGpaint getPaint(NVGcontext *vg, NSVGpaint *p)
 {
-    float sx = grad->xform[4];
-    float sy = grad->xform[5];
-    float ex = sx + grad->xform[2];
-    float ey = sy + grad->xform[3];
-    NVGcolor cs = nvgColorU32(grad->stops[0].color);
-    NVGcolor ce = nvgColorU32(grad->stops[1].color);
+    assert(p->type == NSVG_PAINT_LINEAR_GRADIENT || p->type == NSVG_PAINT_RADIAL_GRADIENT);
+    NSVGgradient *g = p->gradient;
+    assert(g->nstops >= 1);
+    NVGcolor icol = getNVGColor(g->stops[0].color);
+    NVGcolor ocol = getNVGColor(g->stops[g->nstops - 1].color);
 
-    nvgLinearGradient(vg, sx, sy, ex, ey, cs, ce);
+    float inverse[6];
+    nvgTransformInverse(inverse, g->xform);
+    Vec s, e;
+    // Is it always the case that the gradient should be transformed from (0, 0) to (0, 1)?
+    nvgTransformPoint(&s.x, &s.y, inverse, 0, 0);
+    nvgTransformPoint(&e.x, &e.y, inverse, 0, 1);
+
+    NVGpaint paint;
+    if (p->type == NSVG_PAINT_LINEAR_GRADIENT)
+        paint = nvgLinearGradient(vg, s.x, s.y, e.x, e.y, icol, ocol);
+    else
+        paint = nvgRadialGradient(vg, s.x, s.y, 0.0, 160, icol, ocol);
+    return paint;
 }
 
-void nvgDrawSVG(NVGcontext *vg, NSVGimage *image)
-{
-    DISTRHO_SAFE_ASSERT(image != nullptr)
+/** Returns the parameterized value of the line p2--p3 where it intersects with p0--p1 */
+float getLineCrossing(Vec p0, Vec p1, Vec p2, Vec p3) {
+	Vec b = p2.minus(p0);
+	Vec d = p1.minus(p0);
+	Vec e = p3.minus(p2);
+	float m = d.x * e.y - d.y * e.x;
+	// Check if lines are parallel, or if either pair of points are equal
+	if (fabsf(m) < 1e-6)
+		return NAN;
+	return -(d.x * b.y - d.y * b.x) / m;
+}
 
-    for (NSVGshape *shape = image->shapes; shape != NULL; shape = shape->next)
+void nvgDrawSVG(NVGcontext *vg, NSVGimage *svg)
+{
+    DISTRHO_SAFE_ASSERT(svg != nullptr)
+
+    int shapeIndex = 0;
+
+    for (NSVGshape *shape = svg->shapes; shape; shape = shape->next, shapeIndex++)
     {
+        if (!(shape->flags & NSVG_FLAGS_VISIBLE))
+            continue;
+
+        nvgSave(vg);
+
+        if (shape->opacity < 1.0)
+            nvgGlobalAlpha(vg, shape->opacity);
+
         nvgBeginPath(vg);
 
-        nvgStrokeWidth(vg, shape->strokeWidth);
-
-        uint32_t color = shape->stroke.color;
-        NVGcolor ncolor = nvgColorU32(color);
-        nvgStrokeColor(vg, ncolor);
-
-        for (NSVGpath *path = shape->paths; path != NULL; path = path->next)
+        for (NSVGpath *path = shape->paths; path; path = path->next)
         {
             nvgMoveTo(vg, path->pts[0], path->pts[1]);
 
-            for (int i = 0; i < path->npts - 1; i += 3)
+            for (int i = 1; i < path->npts; i += 3)
             {
-                float *p = &path->pts[i * 2];
-                nvgBezierTo(vg, p[2], p[3], p[4], p[5], p[6], p[7]);
+                float *p = &path->pts[2 * i];
+                nvgBezierTo(vg, p[0], p[1], p[2], p[3], p[4], p[5]);
             }
 
             if (path->closed)
+                nvgClosePath(vg);
+
+            // Compute whether this is a hole or a solid.
+            // Assume that no paths are crossing (usually true for normal SVG graphics).
+            // Also assume that the topology is the same if we use straight lines rather than Beziers (not always the case but usually true).
+            // Using the even-odd fill rule, if we draw a line from a point on the path to a point outside the boundary (e.g. top left) and count the number of times it crosses another path, the parity of this count determines whether the path is a hole (odd) or solid (even).
+            int crossings = 0;
+
+            Vec p0 = Vec(path->pts[0], path->pts[1]);
+            Vec p1 = Vec(path->bounds[0] - 1.0, path->bounds[1] - 1.0);
+
+            // Iterate all other paths
+            for (NSVGpath *path2 = shape->paths; path2; path2 = path2->next)
             {
-                nvgLineTo(vg, path->pts[0], path->pts[1]);
+                if (path2 == path)
+                    continue;
 
-                if (shape->fill.type == NSVG_PAINT_COLOR)
-                {
-                    nvgFillColor(vg, nvgColorU32(shape->fill.color));
-                }
-                else if (shape->fill.type == NSVG_PAINT_LINEAR_GRADIENT)
-                {
-                    nvgSVGLinearGrad(vg, shape->fill.gradient);
-                }
+                // Iterate all lines on the path
+                if (path2->npts < 4)
+                    continue;
 
-                if (shape->fill.type != NSVG_PAINT_NONE)
+                for (int i = 1; i < path2->npts + 3; i += 3)
                 {
-                    nvgFill(vg);
+                    float *p = &path2->pts[2 * i];
+
+                    // The previous point
+                    Vec p2 = Vec(p[-2], p[-1]);
+                    // The current point
+                    Vec p3 = (i < path2->npts) ? Vec(p[4], p[5]) : Vec(path2->pts[0], path2->pts[1]);
+
+                    float crossing = getLineCrossing(p0, p1, p2, p3);
+                    float crossing2 = getLineCrossing(p2, p3, p0, p1);
+
+                    if (0.0 <= crossing && crossing < 1.0 && 0.0 <= crossing2)
+                    {
+                        crossings++;
+                    }
                 }
             }
 
-            if (shape->stroke.type != NSVG_PAINT_NONE)
-            {
-                nvgStroke(vg);
-            }
+            if (crossings % 2 == 0)
+                nvgPathWinding(vg, NVG_SOLID);
+            else
+                nvgPathWinding(vg, NVG_HOLE);
         }
-    }
 
-    //nvgStroke(vg);
+        // Fill shape
+        if (shape->fill.type)
+        {
+            switch (shape->fill.type)
+            {
+            case NSVG_PAINT_COLOR:
+            {
+                NVGcolor color = getNVGColor(shape->fill.color);
+                nvgFillColor(vg, color);
+            }
+            break;
+            case NSVG_PAINT_LINEAR_GRADIENT:
+            case NSVG_PAINT_RADIAL_GRADIENT:
+            {
+                NSVGgradient *g = shape->fill.gradient;
+                (void)g;
+                for (int i = 0; i < g->nstops; i++)
+                {
+                }
+
+                nvgFillPaint(vg, getPaint(vg, &shape->fill));
+            }
+            break;
+            }
+
+            nvgFill(vg);
+        }
+
+        // Stroke shape
+        if (shape->stroke.type)
+        {
+            nvgStrokeWidth(vg, shape->strokeWidth);
+            nvgLineCap(vg, (NVGlineCap)shape->strokeLineCap);
+            nvgLineJoin(vg, (int)shape->strokeLineJoin);
+
+            switch (shape->stroke.type)
+            {
+            case NSVG_PAINT_COLOR:
+            {
+                NVGcolor color = getNVGColor(shape->stroke.color);
+                nvgStrokeColor(vg, color);
+            }
+            break;
+            case NSVG_PAINT_LINEAR_GRADIENT:
+            {
+            }
+            break;
+            }
+            nvgStroke(vg);
+        }
+
+        nvgRestore(vg);
+    }
 }
 
 }; // namespace SVGUtils
